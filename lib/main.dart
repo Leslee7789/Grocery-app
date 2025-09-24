0c9ceb4b547288c1c1fb0db3fe3807e0b1ecf552
Leslee7789 <lesliefleischer8@gmail.com>
2025-09-23 22:58:19 -0700

    add home page and image assets

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:grocery_app/pages/homepage.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Homepage(),
+    );
+  }
+}
