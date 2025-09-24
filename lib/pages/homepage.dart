97d62ab58455a32dd402c2094c546dcd91f5961b
Leslee7789 <lesliefleischer8@gmail.com>
2025-09-24 07:12:56 -0700

    add logo and Texts

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -5,6 +5,23 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(80.0),
+            child: Image.asset('lib/images/avocado.jpg'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Text('We deliver groceries at your doorstep' ,textAlign: TextAlign.center,
+            style:
+             TextStyle(
+              fontSize: 40,fontWeight: FontWeight.bold,
+            ),),
+          ),
+        ],
+      ),
+    );
   }
 }
